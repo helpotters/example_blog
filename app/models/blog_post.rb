fc7925f1ff156d8b007c5400080f18ef38bcd5d1
Paul Lemus <paullemus@protonmail.com>
2025-10-26 01:02:30 -0700

    feat: added actiontext

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -4,7 +4,20 @@ class BlogPost < ApplicationRecord
   validates :body, presence: true
   validates :summary, presence: true
 
-  scope :draft, -> (where(published_at: nil))
-  scope :draft, -> (where("published_at <= ?", Time.current )
-  scope :draft, -> (where("published_at > ?", Time.current )
+  scope :sorted, -> { order(published_at: :desc, updated_at: :desc)}
+  scope :draft, -> { where(published_at: nil)}
+  scope :published, -> { where("published_at <= ?", Time.current)}
+  scope :scheduled, -> { where("published_at > ?", Time.current)}
+
+  def draft?
+    published_at.nil? 
+  end
+
+  def published?
+    published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
 end
